61d9dfb30cde7a9f72968e1b290637211ece7977
Ketan <ketantoo@gmail.com>
2020-05-15 20:21:10 +0530

    Image picker added

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -50,6 +50,14 @@ class _AuthFormState extends State<AuthForm> {
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
+                  CircleAvatar(
+                    radius: 40.0,
+                  ),
+                  FlatButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.image),
+                    label: Text("Add Image"),
+                  ),
                   TextFormField(
                     key: ValueKey('email'),
                     validator: (value) {
